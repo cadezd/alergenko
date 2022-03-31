2ccfd2866dc098c58b0f19d75985e9aeb2bd845d
David-api <58308216+David-api@users.noreply.github.com>
2022-03-31 16:16:23 +0200

    added status text textview

--- app/src/main/java/com/example/alergenko/ScanFragment.java
+++ app/src/main/java/com/example/alergenko/ScanFragment.java
@@ -1,6 +1,9 @@
 package com.example.alergenko;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +12,56 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
+
+import java.util.List;
+
 public class ScanFragment extends Fragment {
+
+    private CompoundBarcodeView barcodeView;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.scan_fragement, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        if (container == null) {
+            return null;
+        }
+
+        View v = inflater.inflate(R.layout.scan_fragement, container, false);
+        barcodeView = (CompoundBarcodeView) v.findViewById(R.id.barcode_scanner);
+        barcodeView.setStatusText("");
+        barcodeView.decodeContinuous(callback);
+
+        return v;
+    }
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText() != null) {
+                // TODO: hadle scanning reasult
+            }
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    @Override
+    public void onResume() {
+        barcodeView.resume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        barcodeView.pause();
+        super.onPause();
     }
 }
